5b277fdf8f5b66b2de6a085d5c6aa45c696e7398
Jeff Norton <jeff12ntn@gmail.com>
2016-10-26 10:44:37 -0600

    Basic button setup done

--- ProgrammaticConstraintsWithAnimation/ViewController.swift
+++ ProgrammaticConstraintsWithAnimation/ViewController.swift
@@ -9,17 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //==================================================
+    // MARK: - Properties
+    //==================================================
+    
+    let topLeftButton = UIButton()
+    let topRightButton = UIButton()
+    let bottomLeftButton = UIButton()
+    let bottomRightButton = UIButton()
+    
+    //==================================================
+    // MARK: - General
+    //==================================================
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupButtons()
     }
+    
+    //==================================================
+    // MARK: - Methods
+    //==================================================
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupButtons() {
+        
+        // Set colors
+        
+        topLeftButton.backgroundColor = .green
+        topRightButton.backgroundColor = .yellow
+        bottomLeftButton.backgroundColor = .blue
+        bottomRightButton.backgroundColor = .red
+        
+        // Add targets to buttons for color animation
+        
+        topLeftButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        topRightButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        bottomLeftButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        bottomRightButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        // Add targets to buttons for shake animation
+        
+        topLeftButton.addTarget(self, action: #selector(ViewController.buttonExited), for: .touchDragExit)
+        topRightButton.addTarget(self, action: #selector(ViewController.buttonExited), for: .touchDragExit)
+        bottomLeftButton.addTarget(self, action: #selector(ViewController.buttonExited), for: .touchDragExit)
+        bottomRightButton.addTarget(self, action: #selector(ViewController.buttonExited), for: .touchDragExit)
+        
+        // Add buttons as a subview of the view
+        
+        view.addSubview(topLeftButton)
+        view.addSubview(topRightButton)
+        view.addSubview(bottomLeftButton)
+        view.addSubview(bottomRightButton)
     }
-
-
+    
+    func buttonTapped() {
+        
+        
+    }
+    
+    func buttonExited() {
+        
+        
+    }
 }
+
+
+
+
+
+
 
