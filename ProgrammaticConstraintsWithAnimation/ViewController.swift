8176c36b98bb811efe22792c529a78a47f4da984
Jeff Norton <jeff12ntn@gmail.com>
2016-10-26 11:39:49 -0600

    Shake animation done

--- ProgrammaticConstraintsWithAnimation/ViewController.swift
+++ ProgrammaticConstraintsWithAnimation/ViewController.swift
@@ -147,9 +147,25 @@ class ViewController: UIViewController {
         }
     }
     
-    func buttonExited() {
+    func buttonExited(_ sender: UIButton) {
         
+        let position1 = sender.frame.origin.x + sender.frame.width / 2
+        let position2 = sender.frame.origin.x + sender.frame.width / 2 - 15
+        let position3 = sender.frame.origin.x + sender.frame.width / 2
+        let position4 = sender.frame.origin.x + sender.frame.width / 2 + 15
+        let position5 = sender.frame.origin.x + sender.frame.width / 2
         
+        view.bringSubview(toFront: sender)
+        
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [position1, position2, position3, position4, position5]
+        
+        animation.keyTimes = [0, 0.25, 0.5, 0.75, 1]
+        animation.duration = 0.5
+        animation.repeatCount = 5.0
+        
+        sender.layer.add(animation, forKey: "shake")
     }
 }
 
